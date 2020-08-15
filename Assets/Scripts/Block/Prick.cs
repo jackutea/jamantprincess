b2926dfed9c694ab7ff4f6c7c44117cab96967fb
杰克有茶 <chenwansal1@163.com>
2020-08-16 01:52:59 +0800

    修改: Prick 刺现在可以自动切换伸缩

--- Assets/Scripts/Block/Prick.cs
+++ Assets/Scripts/Block/Prick.cs
@@ -8,6 +8,53 @@ namespace Jam {
 
     public class Prick : BlockBase {
 
+        public Sprite normal;
+        public Sprite hiding;
+        public bool startHiding;
+        public bool isStatic = true;
+        public float gapTime;
+        Collider2D col;
+        Sequence action;
+
+        protected override void Awake() {
+
+            base.Awake();
+
+            col = GetComponent<Collider2D>();
+
+            if (startHiding) {
+
+                sr.sprite = hiding;
+
+            } else {
+
+                sr.sprite = normal;
+
+            }
+
+            if (!isStatic) {
+
+                action = DOTween.Sequence();
+                action.AppendInterval(gapTime);
+                action.AppendCallback(SwitchActivated);
+                action.SetLoops(-1);
+
+            }
+
+        }
+
+        void SwitchActivated() {
+
+            if (sr.sprite == hiding) {
+                sr.sprite = normal;
+                col.enabled = true;
+            } else if (sr.sprite == normal) {
+                sr.sprite = hiding;
+                col.enabled = false;
+            }
+
+        }
+
         protected override void PlayerEnter(ActorBase _actor) {
 
             _actor.Dead();
