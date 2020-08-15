458eb80099b03bf1fc891266962b975bae0b55f8
杰克有茶 <chenwansal1@163.com>
2020-08-16 00:56:06 +0800

    修改: Trampoline 弹簧素材 和 动画

--- Assets/Scripts/Block/Trampoline.cs
+++ Assets/Scripts/Block/Trampoline.cs
@@ -9,19 +9,30 @@ namespace Jam {
     public class Trampoline : BlockBase {
 
         public Sprite waiting;
-        public Sprite activated;
+        public Sprite bounceUp;
+        public Sprite startDown;
+        public Sprite deepDown;
+
         public float bouncePower;
 
         protected override void PlayerEnter(ActorBase _actor) {
 
             _actor.transform.position = new Vector3(_actor.transform.position.x, transform.position.y + 0.4f, _actor.transform.position.z);
 
-            _actor.rig.velocity = new Vector2(_actor.rig.velocity.x, bouncePower);
-            _actor.EnterState(StateType.ForceJump);
+            sr.sprite = startDown;
 
             Sequence _action = DOTween.Sequence();
-            sr.sprite = activated;
-            _action.AppendInterval(0.2f);
+            _action.AppendInterval(0.06f);
+            _action.AppendCallback(() => {
+                sr.sprite = deepDown;
+            });
+            _action.AppendInterval(0.06f);
+            _action.AppendCallback(() => {
+                sr.sprite = bounceUp;
+                _actor.rig.velocity = new Vector2(_actor.rig.velocity.x, bouncePower);
+                _actor.EnterState(StateType.ForceJump);
+            });
+            _action.AppendInterval(0.15f);
             _action.AppendCallback(() => {
                 sr.sprite = waiting;
             });
